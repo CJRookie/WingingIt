366472a9754294b70a21c8ebaedf6c7b505b5c60
CJ <jayyang116@gmail.com>
2023-11-14 15:17:55 -0500

    Initial Commit

---
+++ WingingIt/WingingItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WingingItApp.swift
+//  WingingIt
+//
+//  Created by CJ on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WingingItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
